94fc7a9d704faf9bd404ed994ed1aa34c940b5c0
rsykrm24 <128889699+rsykrm24@users.noreply.github.com>
2025-01-07 19:56:05 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,25 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginRegister; 
 use App\Http\Controllers\UserData;
 use App\Http\Controllers\CreateBlog;
+use App\Models\BlogPost; 
+use App\Http\Controllers\BlogData; 
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', fn() => view('index',["datas" => BlogPost::all()]))->name("index");
 
-Route::get('/login', function () {
-    return view('login');
-})->name("login");
+Route::get('/login', fn() => view("login"))->name("login");
 
-Route::get('/register', function () {
-    return view('register');
-})->name("register");
+Route::get('/register', fn() => view('register'))->name("register");
 
 Route::get('/dashboard',[UserData::class,'UserData'])->name("dashboard")->middleware("auth");
 
 Route::get('/createblog',[CreateBlog::class,'BlogPost'])->name("createblog")->middleware("auth");
+
+Route::get("/blog/{id}",[BlogData::class,"BlogId"]);
 
 Route::post('/loginForm', [LoginRegister::class,"LoginForm"]);
 
 Route::post('/registerForm', [LoginRegister::class,"RegisterForm"]);
 
 Route::post('/logoutForm', [LoginRegister::class,"LogoutForm"]);
+
+Route::post('/blogpost', [UserData::class,"ArticlePost"]);
